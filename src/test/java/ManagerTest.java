3f22a5a09d22aa2ad6d50d2e70487746bb571ca7
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-01 18:36:26 +0000

    Added TDD test for getName in ManagerTest class.

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Hamish", "NW1234567B", 55000.00, "Asset Management");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Hamish", manager.getName());
+    }
 }
